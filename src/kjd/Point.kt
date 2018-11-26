f4a62bdba20a31bdfe42750d051749a7f7ad70d0
Haimanot1989 <haimif@yahoo.com>
2018-11-26 17:23:05 +0100

    super concise constructor: changing the var to val makes the properties readonly.

--- src/kjd/Point.kt
+++ src/kjd/Point.kt
@@ -1,15 +1,23 @@
 package kjd
 //adding var to the constructor params creates the prop and initializes them with the coming value
-class Point(var x:Int, var y:Int)
+class Point(val x:Int, val y:Int)
 
 /*
 //equivalent of the code above
 class Point{
-    int x, y;
+    private int x, y;
 
     class Point(int x, int y){
         this.x = x;
         this.y = y;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    public int getY(){
+        return this.y;
     }
 }
 * */
