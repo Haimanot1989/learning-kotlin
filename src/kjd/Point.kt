6bbb0668a9524337adc243d39b186f0cb057c54e
Haimanot1989 <haimif@yahoo.com>
2018-11-26 17:10:24 +0100

    super concise constructor: adding var to the constructor params creates two properties(instance variable with getter and setter in java world).

--- src/kjd/Point.kt
+++ src/kjd/Point.kt
@@ -1,6 +1,15 @@
 package kjd
+//adding var to the constructor params creates the prop and initializes them with the coming value
+class Point(var x:Int, var y:Int)
 
-class Point(xx:Int, yy:Int){
-    var x = xx
-    var y = yy
+/*
+//equivalent of the code above
+class Point{
+    int x, y;
+
+    class Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
 }
+* */
