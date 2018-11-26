4db367de31f5954bcfe7fce86ec393fabb682b13
Haimanot1989 <haimif@yahoo.com>
2018-11-26 17:27:15 +0100

    Kotlin's seamless interoperability with Java, and calling the get method we never created

---
+++ src/kjd/MainJ.java
@@ -0,0 +1,9 @@
+package kjd;
+
+public class MainJ {
+    public static void main(String[] args) {
+        Point p1 = new Point(3,3);
+        System.out.println(p1.getX());
+        System.out.println(p1.getY());
+    }
+}
